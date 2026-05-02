7121a75a05414599d9c57d0d825f6faea7155e9e
DIPAK <DIPAK@192.168.1.35>
2026-05-02 23:25:36 +0530

    new class code added

---
+++ ZOrangeHRM29April2027/src/test/java/com/tutorialsninja/testcases/Demogit.java
@@ -0,0 +1,14 @@
+package com.tutorialsninja.testcases;
+
+import io.opentelemetry.exporter.logging.SystemOutLogRecordExporter;
+
+public class Demogit {
+
+	public static void main(String[] args) {
+
+
+		
+		System.out.println("hello world");
+	}
+
+}
